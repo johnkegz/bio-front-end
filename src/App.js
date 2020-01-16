0d9ac1530deaf1db8c56d31d58838a685e05d22d
johnkegz <johnkal24@gmail.com>
2020-01-16 10:57:53 +0300

    activate create bio feature

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Home from './components/Home/Home';
 // import Gallery from './components/Gallery';
 // import Board from './components/Board';
 // import Content from './components/Content/Content';
-// import Forms from './components/Forms/Forms';
+import Forms from './components/Forms/Forms';
 import DashBoard from './components/Dashboard';
 import Login from './components/Login';
 import Feeds from './components/Forms/Feeds';
@@ -49,8 +49,8 @@ if(localStorageData) {
           <Route exact path='/obituaries' component={Obituaries}/>
           <Route exact path='/gallery' component={Gallery}/>
           <Route exact path='/board' component={Board}/>
-          <Route exact path='/content/:id' component={Content}/>
-          <Route exact path='/forms' component={Forms}/> */}
+          <Route exact path='/content/:id' component={Content}/> */}
+          <Route exact path='/forms' component={Forms}/>
           <Route exact path='/' component={Login}/>
           <Route path='/login' component={Login}/>
           <Route path='/analytics' component={Analytics}/>
